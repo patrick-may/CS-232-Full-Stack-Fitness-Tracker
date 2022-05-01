2099dcafffcf9cb497b25fa05981f7f6514e1ece
Patrick May <pmay24@wooster.edu>
2022-05-01 15:45:03 -0400

    slow progress on db init

--- app/utils/db.py
+++ app/utils/db.py
@@ -27,15 +27,29 @@ def init_db(config):
     cursor.execute(f"DROP DATABASE IF EXISTS {config['DATABASE']};")
     cursor.execute(f"CREATE DATABASE {config['DATABASE']};")
     cursor.execute(f"use {config['DATABASE']};")
+    #cursor.execute(
+    #    f""" 
+    #    CREATE TABLE tasks
+    #    (
+    #        id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+    #        description VARCHAR(50),
+    #        creation_datetime timestamp,
+    #        completed TINYINT(1),
+    #        CONSTRAINT pk_todo PRIMARY KEY (id)
+    #    );
+    #    """
+    #)
+    #member table init
     cursor.execute(
         f""" 
-        CREATE TABLE tasks
+        CREATE TABLE members
         (
-            id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
-            description VARCHAR(50),
+            gym_id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+            first_name VARCHAR(50),
+            last_name VARCHAR(50),
             creation_datetime timestamp,
-            completed TINYINT(1),
-            CONSTRAINT pk_todo PRIMARY KEY (id)
+            age TINYINT UNSIGNED, 
+            PRIMARY KEY (gym_i
         );
         """
     )
