702f362b5c14e126ceb196ff69c1398e3706c219
Patrick May <pmay24@wooster.edu>
2022-05-09 23:41:31 -0400

    Monday night final commit

--- app/views/crud_lifter_view.py
+++ app/views/crud_lifter_view.py
@@ -4,6 +4,8 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 from datetime import timedelta
 import click
 from models.gym_member import Gym_Member, Gym_Member_DB
+from models.weight_set import Weight_Set, Weight_Set_DB
+from models.workout import Workout, WorkoutDB
 
 crud_lifter_blueprint = Blueprint('crud_lifter_blueprint', __name__)
 
@@ -28,11 +30,19 @@ def login():
 
 @crud_lifter_blueprint.route("/user")
 def user():
+    gym_db =  Gym_Member_DB(g.mysql_db, g.mysql_cursor)
+
     if "user" in session:
         user = session["user"]
+        
         if user == "admin":
-            return render_template("admin.html", user=user)
-        return render_template("user.html", user=user)
+            user_list = gym_db.select_all_members()
+            return render_template("admin.html", user=user, gym_member_list=user_list)
+        
+        weight_db = Weight_Set_DB(g.mysql_db, g.mysql_cursor)
+        user_sets = weight_db.select_all_exercises()
+        username = gym_db.select_individual_member(user)[0]["name"]
+        return render_template("user.html", user=username, member_sets=user_sets)
 
     else:
         flash("You are not logged in")
@@ -63,7 +73,7 @@ def member_creation():
 
     add_db.insert_individual_member(new_member)
 
-    return redirect("/")
+    return redirect("/user")
 
 
 @crud_lifter_blueprint.route('/delete_member')
@@ -75,69 +85,7 @@ def remove_member():
     user_id = request.form.get("member_id")
     delete_db = Gym_Member_DB(g.mysql_db, g.mysql_cursor)
     delete_db.delete_member(user_id)
-    return redirect("/")
-
-
-"""
-@crud_lifter_blueprint.route("/", methods = ["GET", "POST"])
-def home_page():
-    memberDB = Gym_Member_DB(g.mysql_db, g.mysql_cursor)
-
-    display_data = memberDB.select_all_members()
-
-    setDB = Weight_Set_DB(g.mysql_db, g.mysql_cursor)
-
-    weight_data = setDB.select_all_exercises()
-
-    return render_template("home-page.html", data=display_data, weight_data=weight_data)
-    #return render_template("home-page.html", data=display_data)
-
-
-Incomplete login thing
-@crud_lifter_blueprint("/login", methods = ["GET", "POST"])
-def login():
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        login_user(user)
-        click.echo("Logged in successfully")
-        next = Flask.request.args.get('next')
-
-        if not is_safe_url(next):
-            return Flask.abort(400)
-        
-        return 
-
-
-
-@crud_lifter_blueprint.route('/create_member')
-def gather_member_info():
-   return render_template("member-entry.html")
-
-@crud_lifter_blueprint.route("/add_member", methods=["POST"])
-def member_creation():
-    new_member_name = request.form.get("member_name")
-    new_member_age = request.form.get("member_age")
-    new_member_sex = request.form.get("member_sex")
-
-    new_member = Gym_Member(new_member_name, new_member_age, new_member_sex)
-    add_db = Gym_Member_DB(g.mysql_db, g.mysql_cursor)
-
-    add_db.insert_individual_member(new_member)
-
-    return redirect("/")
-
-@crud_lifter_blueprint.route('/delete_member')
-def member_deletion():
-    return render_template("member-deletion.html")
-
-@crud_lifter_blueprint.route("/remove_account", methods=["POST"])
-def remove_member():
-    user_id = request.form.get("member_id")
-    delete_db = Gym_Member_DB(g.mysql_db, g.mysql_cursor)
-
-    delete_db.delete_member(user_id)
-    return redirect("/")
+    return redirect("/user")
 
 @crud_lifter_blueprint.route("/create_weight_set")
 def gather_set_info():
@@ -146,11 +94,14 @@ def gather_set_info():
 @crud_lifter_blueprint.route("/add_weight_set", methods=["POST"])
 def set_creation():
     machine_name = request.form.get("machine_name")
-    user_id = request.form.get("user_id")
+    if "user" in session:
+        user_id = session["user"]
+    else:
+        user_id = 0
+
     date = request.form.get("date")
     reps = request.form.get("reps")
     weight = request.form.get("weight")
     setDB = Weight_Set_DB(g.mysql_db, g.mysql_cursor)
     setDB.insert_weight_set(Weight_Set(machine_name, user_id, date, reps, weight))
-    return redirect("/")
-    """
+    return redirect("/user")
